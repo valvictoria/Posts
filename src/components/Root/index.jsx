cce3751f4d98f3ab0193374c8c146b94321e9392
valvictoriya <kareidly@gmail.com>
2025-05-10 22:55:29 +0500

    Add auth

--- src/components/Root/index.jsx
+++ src/components/Root/index.jsx
@@ -1,17 +1,32 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../../redux/slices/authSlice";
 import { Container } from '../ui/Container'
+import { Button } from "../ui/Button";
 import * as SC from "./styles";
 
+
+
 export const Root = () => {
+    const { user } = useSelector((state) => state.auth)
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+
+    const onClickExitBtn = () => {
+        dispatch(logout())
+        navigate('/auth')
+    }
+
     return (
         <>
             <Container>
                 <SC.Menu>
                     <SC.MenuItem to={'/'}>Главная</SC.MenuItem>
                     <SC.MenuItem to={'/posts'}>Список постов</SC.MenuItem>
-                    <SC.MenuItem to={'/posts/add'}>Добавление поста</SC.MenuItem>
-                    <SC.MenuItem to={'/auth'}>Авторизация</SC.MenuItem>
-                    <SC.MenuItem to={'/registration'}>Регистрация</SC.MenuItem>
+                    {user && <SC.MenuItem to={'/posts/add'}>Добавление поста</SC.MenuItem>}
+                    {!user && <SC.MenuItem to={'/auth'}>Авторизация</SC.MenuItem>}
+                    {!user && <SC.MenuItem to={'/registration'}>Регистрация</SC.MenuItem>}
+                    {user && <Button onClick={onClickExitBtn}>Выход</Button>}
                 </SC.Menu>
             </Container>
             <Outlet />
--- src/pages/auth/index.jsx
+++ src/pages/auth/index.jsx
@@ -1,2 +1,78 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import { useDispatch } from "react-redux"
+import { login } from '../../redux/slices/authSlice'
+import { Container } from "../../components/ui/Container"
+import { Typo } from "../../components/ui/Typo"
+import { Form } from "../../components/ui/Form"
+import { Field } from "../../components/ui/Field"
+import { Input } from "../../components/ui/Input"
+import { Button } from "../../components/ui/Button"
 
-export const AuthPage = () => <>Авторизация</>
+export const AuthPage = () => {
+    const [formValues, setFormValues] = useState(
+        {
+            email: '',
+            password: ''
+        }
+    )
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+
+    const onChange = (name, value) => {
+        setFormValues({ ...formValues, [name]: value })
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+        try {           
+            const users = JSON.parse(localStorage.getItem('users'))
+
+            if (!users) {
+                alert('Данный пользователь не найден в системе')
+                return
+            }
+
+            const currentUser = users.find((user) => user.email === formValues.email && user.password === formValues.password)
+
+            if (!currentUser) {
+                alert('Данный пользователь не найден в системе')
+                return
+            }
+
+            dispatch(login(currentUser))
+            navigate('/posts')
+
+        } catch (e) {
+            console.log(e)
+        }
+        
+    }
+
+    const disabled = !formValues.email || !formValues.password
+
+    return <Container>
+        <Typo>Авторизация</Typo>
+        <Form onSubmit={onSubmit} autoComplete="off">
+            <Field>
+                <Input
+                    type="email"
+                    name="email"
+                    placeholder='E-mail'
+                    value={formValues.email}
+                    onChange={(e) => onChange(e.target.name, e.target.value)}
+                    autoComplete="off" />
+            </Field>
+            <Field>
+                <Input
+                    type="password"
+                    name="password"
+                    placeholder='Пароль'
+                    value={formValues.password}
+                    onChange={(e) => onChange(e.target.name, e.target.value)}
+                    autoComplete="off" />
+            </Field>
+            <Button type='submit' disabled={disabled}>Авторизация</Button>
+        </Form>
+    </Container>
+}
